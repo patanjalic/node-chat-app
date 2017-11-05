53810f776799a226d60489434605fd836c748e2c
Patanjali C <patanjalic@github.com>
2017-11-04 21:17:08 -0400

    adding get

--- server/app.js
+++ server/app.js
@@ -5,6 +5,10 @@ var app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname,'../public')));
 
+app.get('/',(req,res)=>{
+  res.sendFile(path.join(__dirname,'../public/index.html'));
+});
+
 app.listen(port,() => {
   console.log(`App successfully started on port ${port}`);
 });
